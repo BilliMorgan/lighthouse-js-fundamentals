0a79fff25c084471cbf61a9b3a8c84f3ad116f1f
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-08-13 07:39:42 -0400

    ruby in 20 minutes

---
+++ rubyTinker/ex1.rb
@@ -0,0 +1,16 @@
+puts "Hello World!"
+# puts "Hello Again"
+
+puts "is it less or equal? #{5<= -2}"
+puts 3 + 2 < 5 - 7 
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+cars_not_driven = cars - drivers
+cars_driven = drivers
+puts cars_not_driven
+
+carpool_capacity = cars_driven * space_in_a_car
+
+puts "we have #{carpool_capacity}"
